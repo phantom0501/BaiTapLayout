c76f5241716bd2acc8431e267b017cf6182795fa
LeeCuong2k <letrungcuong2k@gmail.com>
2021-02-04 08:49:02 +0700

    edit file App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import "./App.css";
 import BaiTapLayoutNew from "./BaiTapLayoutNew/BaiTapLayoutNew";
-import BaiTapThucHanhLayout from "./BaiTapThucHanhLayout/BaiTapThucHanhLayout";
 
 function App() {
   return <BaiTapLayoutNew />;
-  // <BaiTapThucHanhLayout />
 }
 
 export default App;
